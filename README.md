47149f05b970dde24f1ed6e46f1082a56a53c7e2
Lukas Kahwe Smith <smith@pooteeweet.org>
2011-11-12 12:26:31 +0100

    added travis-ci

--- README.md
+++ README.md
@@ -7,3 +7,4 @@ http://github.com/FriendsOfSymfony/FOSRest
 The main goals for now are to provide ways for decoding request bodies
 and request format negotiation (inspired by Apache mod_negotiation)
 
+[![Build Status](https://secure.travis-ci.org/FriendsOfSymfony/FOSRest.png)](http://travis-ci.org/FriendsOfSymfony/FOSRest)
--- Tests/bootstrap.php
+++ Tests/bootstrap.php
@@ -1,21 +1,16 @@
 <?php
 
-require_once $_SERVER['SYMFONY'].'/Symfony/Component/ClassLoader/UniversalClassLoader.php';
+/*
+ * This file is part of the FOSRestBundle package.
+ *
+ * (c) FriendsOfSymfony <http://friendsofsymfony.github.com/>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-use Symfony\Component\ClassLoader\UniversalClassLoader;
-
-$loader = new UniversalClassLoader();
-$loader->registerNamespace('Symfony', $_SERVER['SYMFONY']);
-$loader->register();
-
-spl_autoload_register(function($class)
-{
-    if (0 === strpos($class, 'FOS\\Rest\\')) {
-        $path = __DIR__.'/../'.implode('/', array_slice(explode('\\', $class), 2)).'.php';
-        if (!stream_resolve_include_path($path)) {
-            return false;
-        }
-        require_once $path;
-        return true;
-    }
-});
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
